f2e41503e9ab2874b2ae4b16d55fcceac49d6afe
Joshua Strodtbeck <Find me on LinkedIn>
2023-09-09 15:56:12 -0500

    Added geometry to mesh

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -4,37 +4,49 @@
 #include <stdexcept>
 
 #include <iostream>
+#include <cmath>
 
 class SquareTriCSRMesh::CSR
 {
     public:
-    CSR(int size) : _nbrDispl(size + 1)
+    CSR(int size) : 
+    _nbrDispl(size + 1),
+    _area(size + 1)
     {}
 
     void setNbrSize(int size)
-    { _nbr.resize(size); }
+    { 
+        _nbr.resize(size); 
+        _length.resize(size); 
+    }
 
     void inline setDispl(int i, int value)
     { _nbrDispl[i] = value; }
 
-    void inline setNbr(int displ, int nbrIndex)
-    { _nbr[displ] = nbrIndex; }
+    void inline setNbr(int displ, int nbrIndex, float length)
+    {
+        _nbr[displ] = nbrIndex;
+        _length[displ] = length;
+    }
 
-    void inline setNbr(int i, int iNbr, int nbrIndex)
-    { _nbr[_nbrDispl[i] + iNbr] = nbrIndex; }
+    void inline setArea(int i, float area)
+    { _area[i] = area; }
+
 
     std::size_t size()
     { return _nbrDispl.size() - 1; }
 
     std::span<int> getNbr(int i)
     {
-//        std::cout << "_nbrDispl[" << i << "]: " << _nbrDispl[i] << "\n";
         return std::span<int>(&_nbr[_nbrDispl[i]], _nbrDispl[i+1] - _nbrDispl[i]);
     }
 
 private:
     std::vector<int> _nbrDispl;
     std::vector<int> _nbr;
+
+    std::vector<float> _length;
+    std::vector<float> _area;
 };
 
 
@@ -76,11 +88,16 @@ void SquareTriCSRMesh::setIndices()
 
     csr.setNbrSize(3*csr.size());
 
-
     int displ = 0;
     //This is not really very efficient
     //Look at all those branches inside this loop
     //Really not something you can run on a GPU
+
+    float const length = 1.0 / _nRows;
+    float const width  = 1.0 / _nCols;
+    float const hyp    = sqrt(length * length + width * width);
+    float area = 0.5 * length * width;
+
     for (int i = 0; i < _nRows; ++i)
     {
         for (int j = 0; j < _nCols; ++j)
@@ -88,15 +105,17 @@ void SquareTriCSRMesh::setIndices()
             int lower = 2*(i*_nCols + j);
             int upper = 2*(i*_nCols + j) + 1;
 
+            csr.setArea(lower, area);
             csr.setDispl(lower, displ);
-            if (j != 0)        csr.setNbr(displ++, lower - 1);
-                               csr.setNbr(displ++, lower + 1);
-            if (i != _nRows-1) csr.setNbr(displ++, lower + (2 * _nCols + 1));
+            if (j != 0)        csr.setNbr(displ++, lower - 1, length);
+                               csr.setNbr(displ++, lower + 1, hyp);
+            if (i != _nRows-1) csr.setNbr(displ++, lower + (2 * _nCols + 1), width);
 
+            csr.setArea(upper, area);
             csr.setDispl(upper, displ);
-                               csr.setNbr(displ++, upper - 1);
-            if (j != _nCols-1) csr.setNbr(displ++, upper + 1);
-            if (i != 0)        csr.setNbr(displ++, upper - (2 * _nCols + 1));
+                               csr.setNbr(displ++, upper - 1, hyp);
+            if (j != _nCols-1) csr.setNbr(displ++, upper + 1, length);
+            if (i != 0)        csr.setNbr(displ++, upper - (2 * _nCols + 1), width);
         }
     }
 
